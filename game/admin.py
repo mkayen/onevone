74b9e2c55f85b5f020cf24453901ce0923ad78c1
jogarock <me@joga.rock>
2013-11-20 21:42:11 -0500

    added game models to admin interface

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,9 @@
+import sys
+#sys.path.append('~/Dev/ovo_dev/onevone')
 from django.contrib import admin
-
+from models import Team, Player, User
 # Register your models here.
+
+admin.site.register(Team)
+admin.site.register(Player)
+admin.site.register(User)
